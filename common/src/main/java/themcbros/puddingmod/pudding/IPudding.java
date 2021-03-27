de57ac112bd83fb956ad1a2e2f28fce2d8d23a37
TheMCLoveMan <themcloveman@gmail.com>
2021-03-27 11:40:11 +0100

    Add vanilla and chocolate pudding

---
+++ common/src/main/java/themcbros/puddingmod/pudding/IPudding.java
@@ -0,0 +1,51 @@
+package themcbros.puddingmod.pudding;
+
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BowlFoodItem;
+import net.minecraft.world.item.Item;
+import themcbros.puddingmod.PuddingMod;
+
+public interface IPudding {
+
+    /**
+     * @return Name of pudding sort
+     */
+    String getName();
+
+    /**
+     * This is used for {@link net.minecraft.client.color.item.ItemColors}
+     * @return Color of the pudding
+     */
+    default int getColor() {
+        return -1;
+    }
+
+    /**
+     * Used for {@link FoodProperties} creation
+     * @return Nutrition (default 6)
+     */
+    default int getNutrition() {
+        return 6;
+    }
+
+    /**
+     * Used for {@link FoodProperties} creation
+     * @return Food saturation as float (default 0.6F)
+     */
+    default float getSaturationModifier() {
+        return 0.6F;
+    }
+
+    /**
+     * @param pudding The Pudding
+     * @return {@link BowlFoodItem} with {@link FoodProperties} and creative tab
+     */
+    static BowlFoodItem getPuddingItem(IPudding pudding) {
+        FoodProperties foodProperties = new FoodProperties.Builder()
+                .nutrition(pudding.getNutrition())
+                .saturationMod(pudding.getSaturationModifier())
+                .build();
+        return new BowlFoodItem(new Item.Properties().food(foodProperties).stacksTo(1).tab(PuddingMod.PUDDING_TAB));
+    }
+
+}
---
+++ common/src/main/java/themcbros/puddingmod/pudding/PuddingSorts.java
@@ -0,0 +1,8 @@
+package themcbros.puddingmod.pudding;
+
+public class PuddingSorts {
+
+    public static final IPudding VANILLA = () -> "vanilla";
+    public static final IPudding CHOCOLATE = () -> "chocolate";
+
+}
