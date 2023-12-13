2c1a5931e5cf9ee4f2095bb2cd56cb7e3dbc003e
TheMCLoveMan <themcloveman@gmail.com>
2023-12-13 10:56:11 +0100

    add support for NeoForge

---
+++ neoforge/src/main/java/net/themcbrothers/puddingmod/neoforge/PuddingModNeoForge.java
@@ -0,0 +1,14 @@
+package net.themcbrothers.puddingmod.neoforge;
+
+import net.neoforged.fml.common.Mod;
+import net.themcbrothers.puddingmod.PuddingMod;
+
+/**
+ * @author TheMCLoveMan
+ */
+@Mod(PuddingMod.MOD_ID)
+public class PuddingModNeoForge {
+    public PuddingModNeoForge() {
+        PuddingMod.init();
+    }
+}
