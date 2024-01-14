bbf335f4dd5dc8a720186c4a5124fc4ac486e47f
TheMCLoveMan <themcloveman@gmail.com>
2024-01-14 15:20:17 +0100

    Use ArchitecturyBucketItem, should fix capabilities

--- common/src/main/java/net/themcbrothers/puddingmod/PuddingItems.java
+++ common/src/main/java/net/themcbrothers/puddingmod/PuddingItems.java
@@ -1,5 +1,6 @@
 package net.themcbrothers.puddingmod;
 
+import dev.architectury.core.item.ArchitecturyBucketItem;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
@@ -15,7 +16,7 @@ public class PuddingItems {
     public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(PuddingMod.MOD_ID, Registries.ITEM);
 
     public static final RegistrySupplier<Item> PUDDING_BUCKET = REGISTER.register("pudding_bucket", () ->
-            new BucketItem(PuddingFluids.PUDDING.get(), new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1).arch$tab(PUDDING_TAB)));
+            new ArchitecturyBucketItem(PuddingFluids.PUDDING, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1).arch$tab(PUDDING_TAB)));
     public static final RegistrySupplier<Item> PUDDING_ITEM = REGISTER.register("pudding", () ->
             new BowlFoodItem(new Item.Properties().food(Foods.BEETROOT_SOUP).stacksTo(16).craftRemainder(Items.BOWL).arch$tab(PUDDING_TAB)));
     public static final RegistrySupplier<BowlFoodItem> VANILLA_PUDDING = registerPudding(PuddingVariants.VANILLA);
